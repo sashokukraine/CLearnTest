1efb2a0ceaf2c7616943f5fd1494727e62cc4848
sashokukraine <sashokukraine@gmail.com>
2025-07-06 20:12:17 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string UserName;
+    std::cout << "Hello!\n" "Please Enter your name: ";
+    std::cin >> UserName;
+    std::cout << "Hello, " << UserName << "!";
+}
